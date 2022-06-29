39c61b3335713ac72217d5f7d5d7093656b03e7c
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-06-30 03:35:04 +0600

    cotnact form adding in the cotnact us page

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -1,14 +1,44 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const Contact = () => {
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+    }
     return (
         <>
-        <div className="container">
-            <div className="row">
-                <h2>This is Contact Page</h2>
+            <div className="container my-5">
+                <div className="row py-5">
+                    <h2 className='text-center'>Contact With Us</h2>
+                    <div className="col-md-8 offset-md-2 col-xs-12">
+                    <Form onClick={handleSubmit}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Name *</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Name" />                          
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />                          
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Phone Number</Form.Label>
+                            <Form.Control type="number" placeholder="Phone Number" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Message</Form.Label>
+                            <Form.Control as="textarea" rows={5} placeholder='Your Message ' />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Contact Us
+                        </Button>
+                    </Form>
+                    </div>
+                </div>
             </div>
-        </div>
-            
+
         </>
     );
 };
